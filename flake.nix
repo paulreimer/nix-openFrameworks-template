7dc22387423809d3055a51ec2738797b2aee3dab
Paul Reimer <paul@p-rimes.net>
2022-05-16 00:55:43 -0700

    🔧 Add nix flake definition

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "emptyExample";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-22.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            (import ./nix/overlays/tess2-overlay.nix)
+          ];
+        };
+      in rec {
+        # nix build
+        packages.default = import ./default.nix { inherit pkgs; };
+
+        # nix run
+        apps.default = flake-utils.lib.mkApp { drv = packages.default; };
+      });
+}
